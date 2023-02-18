3b90ee278741cf7d4c0946e34e14b36772409c2e
BKluuis <luisscalabrin@gmail.com>
2023-02-17 22:27:03 -0300

    Added DisplayName for each member and slight change to Order invoice validation

--- e-CommerceUsingModelsAndValidation/Models/Order.cs
+++ e-CommerceUsingModelsAndValidation/Models/Order.cs
@@ -2,20 +2,25 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.ComponentModel.DataAnnotations;
 using e_CommerceUsingModelsAndValidation.CustomValidators;
+using System.ComponentModel;
 
 namespace e_CommerceUsingModelsAndValidation.Models
 {
     public class Order : IValidatableObject
     {
         [BindNever]
+        [DisplayName("Order Number")]
         public int? OrderNo { get; set; }
         [Required]
+        [DisplayName("Order Date")]
         [MinimunYear(2000)]
         public DateTime? OrderDate { get; set; }
         [Required]
+        [DisplayName("Invoice Price")]
         public double? InvoicePrice { get; set; }
         [Required]
-        [MinLength(1, ErrorMessage = "The field 'Products' must contain at least one product")]
+        [DisplayName("List of products")]
+        [MinLength(1, ErrorMessage = "The field '{0}' must contain at least one product")]
         public List<Product> Products { get; set; } = new List<Product>();
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
@@ -23,7 +28,7 @@ namespace e_CommerceUsingModelsAndValidation.Models
             double totalPrice = 0.00;
             foreach(Product product in Products)
             {
-                totalPrice = Convert.ToDouble(product.Price * product.Quantity);
+                totalPrice += Convert.ToDouble(product.Price * product.Quantity);
             }
 
             if(InvoicePrice != totalPrice)
--- e-CommerceUsingModelsAndValidation/Models/Product.cs
+++ e-CommerceUsingModelsAndValidation/Models/Product.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace e_CommerceUsingModelsAndValidation.Models
 {
     public class Product
     {
         [Required]
+        [DisplayName("Product Code")]
         public int? ProductCode { get; set; }
         [Required]
         public double? Price { get; set; }
